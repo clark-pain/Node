03f75290d753912b0c3c814cb8d54bc7faf60675
clark-pain <53712294+clark-pain@users.noreply.github.com>
2020-04-23 09:05:23 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var supervillains = require ("supervillains");
+var superheroes = require("superheroes");
+
+
+var mySupervillainName = supervillains.random();
+var mySuperheroName = superheroes.random();
+
+console.log("My Supervillain name is " + mySupervillainName);
+console.log("My Superhero name is " + mySuperheroName);
